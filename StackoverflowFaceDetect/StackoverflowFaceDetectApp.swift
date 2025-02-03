24bb998368f5c4fadbc2084352f21abc1eeae9f8
flashno <flashno@gmail.com>
2025-02-03 08:48:01 -0800

    Initial Commit

---
+++ StackoverflowFaceDetect/StackoverflowFaceDetectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackoverflowFaceDetectApp.swift
+//  StackoverflowFaceDetect
+//
+//  Created by Rik Basu on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct StackoverflowFaceDetectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
